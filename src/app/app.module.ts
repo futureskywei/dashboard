abbcfd4127d26d914ce2edeecfcb3b1e9313ba95
guoshaowei <guoshaowei@rd.tuan800.com>
2016-12-02 18:06:50 +0800

    init

---
+++ src/app/app.module.ts
@@ -0,0 +1,40 @@
+import { NgModule } from '@angular/core'
+import { BrowserModule } from "@angular/platform-browser";
+import { HttpModule } from "@angular/http";
+import { LocationStrategy, HashLocationStrategy } from '@angular/common';
+
+// App views
+
+// App modules/components
+import { AppComponent } from "./app.component";
+import { MainViewModule } from "./views/main-view/main-view.module";
+import { MinorViewModule } from "./views/minor-view/minor-view.module";
+import { NavigationModule } from "./views/common/navigation/navigation.module";
+import { FooterModule } from "./views/common/footer/footer.module";
+import { TopnavbarModule } from "./views/common/topnavbar/topnavbar.module";
+import { AppRoutingModule } from "./app-routing.module";
+@NgModule({
+  declarations: [AppComponent],
+  imports: [
+
+    // Angular modules
+    BrowserModule,
+    HttpModule,
+
+    // Views
+    MainViewModule,
+    MinorViewModule,
+
+    // Modules
+    NavigationModule,
+    FooterModule,
+    TopnavbarModule,
+
+    AppRoutingModule
+  ],
+  providers: [{ provide: LocationStrategy, useClass: HashLocationStrategy }],
+  bootstrap: [AppComponent]
+})
+
+export class AppModule {
+}
